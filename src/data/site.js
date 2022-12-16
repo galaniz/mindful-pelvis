44e8a331bd47d2c86a76fc1f1cc443abbbb597e3
Graciela Alaniz <alanizcreative@gmail.com>
2022-12-16 00:36:11 -0500

    Env fix

--- src/data/site.js
+++ src/data/site.js
@@ -12,5 +12,6 @@ module.exports = {
   title: 'The Mindful Pelvis',
   description: 'Ontario pelvic health physiotherapy and women\'s health education',
   themeColor: '#f4eae0',
-  styles: `${getPermalink('', true)}/assets/css/mp.css`
+  styles: `${getPermalink('', true)}/assets/css/mp.css`,
+  env: process.env.NODE_ENV
 }
